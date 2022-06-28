fb04361301a66e450ada4472a9c617c3cae6f7d4
nomoreFt <hyunwoo.kim1108@gmail.com>
2022-06-28 22:20:51 +0900

    commit

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,5 +1,6 @@
 package hello.exception.api;
 
+import hello.exception.web.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +19,13 @@ public class ApiExceptionController {
             throw new RuntimeException("잘못된 사용자");
         }
 
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력값");
+        }
+
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
         return new MemberDto(id, "hello" + id);
     }
 
